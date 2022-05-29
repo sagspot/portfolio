dd7ba68f28196c4ae15005fd0f3cf0a03a581e3a
sagspot <ollysag@gmail.com>
2022-05-29 21:52:41 +0300

    remove redundant files

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,5 @@
-import categories from './categories';
 import portfolio from './portfolio';
 import testimonials from './testimonials';
 import users from './users';
 
-export { categories, portfolio, testimonials, users };
+export { portfolio, testimonials, users };
