fafa9dd37eb9c17185758459e56970631379e98a
Oliver Sagala <ollysag@gmail.com>
2023-02-04 18:22:08 +0300

    address type issues

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,4 @@
 import portfolio from './portfolio';
 import testimonials from './testimonials';
-import users from './users';
 
-export { portfolio, testimonials, users };
+export { portfolio, testimonials };
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,5 +1,7 @@
+export {};
+
 declare global {
   interface Window {
-    dataLayer: any;
+    dataLayer: any[];
   }
 }
