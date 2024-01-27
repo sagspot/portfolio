8d9312358dbb3afc8e4b3e98e52cb2cb877f9937
Oliver Sagala <ollysag@gmail.com>
2024-01-27 17:43:14 +0300

    [refactor]: add about

--- src/app/(site)/about.tsx
+++ src/app/(site)/about.tsx
@@ -1,3 +1,46 @@
-export default function About() {
-  return <div>About</div>;
+import { Button } from '@/components/ui/button';
+import { stacks } from '@/data/stacks';
+import Image from 'next/image';
+import Link from 'next/link';
+import { AiOutlineFilePdf } from 'react-icons/ai';
+
+type Props = { resume: string; about: string };
+
+export default function About({ resume, about }: Props) {
+  return (
+    <section
+      id="about"
+      className="container py-4 my-6 max-w-7xl sm:my-8 md:my-10 sm:py-6 md:py-8"
+    >
+      <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-32">
+        <div className="flex flex-col">
+          <h2>About Me</h2>
+
+          <p className="mt-6">{about}</p>
+
+          <div>
+            <Button asChild className="mt-4">
+              <Link href={resume} download target="_blank">
+                <AiOutlineFilePdf className="w-4 h-4 mr-2" />
+                Download my Resume
+              </Link>
+            </Button>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-5 gap-3">
+          {stacks.map((stack) => (
+            <Image
+              key={stack.name}
+              src={stack.icon}
+              alt={stack.name}
+              width={50}
+              height={50}
+              className="object-contain w-auto h-auto"
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,11 +4,18 @@ import Contact from './contact';
 import Hero from './hero';
 import Portfolio from './portfolio';
 
+const about = `I am a professional self-taught software developer based in Nairobi,
+Kenya. I create beautiful professional websites using best practice
+accessibility. I enjoy turning complex problems into simple,
+beautiful and intuitive interface designs. I am passionate about
+transforming lives through tech. I believe tech should ultimately
+make life easier`;
+
 export default function Page() {
   return (
     <React.Fragment>
       <Hero />
-      <About />
+      <About resume="/resume" about={about} />
       <Portfolio />
       <Contact />
     </React.Fragment>
