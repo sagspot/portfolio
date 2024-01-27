2826102eaa159e06d266713ca3cfbd11d4bb62eb
Oliver Sagala <ollysag@gmail.com>
2024-01-27 17:14:20 +0300

    add placeholder of other sections to app

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import About from './about';
+import Contact from './contact';
 import Hero from './hero';
+import Portfolio from './portfolio';
 
 export default function Page() {
   return (
     <React.Fragment>
       <Hero />
+      <About />
+      <Portfolio />
+      <Contact />
     </React.Fragment>
   );
 }
